99df2f9eb15490f1b064956a712dd55c6a176e12
Jessica <jessicagama9827@gmail.com>
2021-09-01 16:19:42 -0400

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+/*==================== SHOW MENU ====================*/
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+
+
+/*==================== REMOVE MENU MOBILE ====================*/
+
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+
+/*==================== SWIPER DISCOVER ====================*/
+
+
+/*==================== VIDEO ====================*/
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+/*==================== SCROLL REVEAL ANIMATION ====================*/
+
+/*==================== DARK LIGHT THEME ====================*/ 
+
+// Previously selected topic (if user selected)
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+
+// We validate if the user previously chose a topic
+
+// Activate / deactivate the theme manually with the button
+
+    // Add or remove the dark / icon theme
+
+    // We save the theme and the current icon that the user chose
